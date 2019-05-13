d941018fffb4738112408b111a4c533cde472a2a
Brant Keener <brant_keener@yahoo.com>
2019-05-13 10:42:17 -0600

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Backend-Demos
+
+![Demo Screen Capture](./assets/images/backend-demo-cap.png)
+
 A landing page for demo videos related to CLI applications.
+
+## Getting Started
+
+Cloning and forking with git are easy. To clone, head over to [Backend-Demos](https://github.com/BrantKeener/Backend-Demos) and navigate using the clone or download button. Copy the link, and perform 
+`git clone {copied link}`.
+
+### Prerequisites
+
+This application runs without any dependencies. After forking or cloning, no further installs will be necessary.
+
+### Installing
+
+This application runs without any dependencies. Simply fork or clone.
+
+## Deployment
+
+This applicaton deploys easily to github pages. Simply navigate to settings in the remote repository, and activate GitHub pages for the application.
+
+## Built With
+
+* [JavaScript](http://es6-features.org/#Constants)
+
+## Contributing
+
+Contributing is currently locked and by invite only.
+
+## Versioning
+
+All versioning is controlled through GitHub
+
+## Authors
+
+* **Brant Keener** - *Primary Author. All JavaScript, and general portfolio direction* - [BrantKeeer](https://github.com/BrantKeener)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- javascript/demo.js
+++ javascript/demo.js
@@ -8,21 +8,21 @@ const clickChecker = event => {
         case 'liri-link':
         player.innerHTML = 
             `<video class="rounded" width="800" height="600" controls>
-                <source src="video/Liri-Bot Demonstration_Medium.mp4" type="video/mp4">
+                <source src="./assets/video/Liri-Bot Demonstration_Medium.mp4" type="video/mp4">
                 Your browser does not support the video tag.
             </video>`;
             break;
         case 'constructor-link':
         player.innerHTML =
             `<video class="rounded" width="800" height="600" controls>
-                <source src="video/Constructor Word Demo_Medium.mp4" type="video/mp4">
+                <source src="./assets/video/Constructor Word Demo_Medium.mp4" type="video/mp4">
                 Your browser does not support the video tag.
             </video>`
         break;
         case 'bamazon-link':
         player.innerHTML =
             `<video class="rounded" width="800" height="600" controls>
-                <source src="video/Bamazon Demo Video_Medium.mp4" type="video/mp4">
+                <source src="./assets/video/Bamazon Demo Video_Medium.mp4" type="video/mp4">
                 Your browser does not support the video tag.
             </video>`
         break;
